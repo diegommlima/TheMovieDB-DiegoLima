1ca6d36f0d0b1652dc9412c7e234e8d987601553
Diego Lima <dmarlon@ciandt.com>
2018-03-11 23:30:26 -0300

    Layout changes, and new extensions

---
+++ TheMovieDB-DiegoLima/Source/Common/Extensions and Protocols/ColorExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ColorExtension.swift
+//  TheMovieDB-DiegoLima
+//
+//  Created by Diego Lima on 09/03/18.
+//  Copyright © 2018 Diego Marlon Medeiros Lima. All rights reserved.
+//
+
+import Foundation
